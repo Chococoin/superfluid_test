355d8e62e9fdd368aefe5158d5384a1eb2cafad6
Germán Lugo <german.lugo@bitnibs.com>
2020-12-28 21:54:20 +0100

    Fix the bug.

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ console.log("🏁 Start ~/tbot/test.js")
 require('dotenv').config()
 const { toWad, toBN, fromWad, wad4human } = require("@decentral.ee/web3-helpers")
 const HDWalletProvider = require("@truffle/hdwallet-provider")
-const TruffleContract = require("@truffle/contract")
 let provider = new HDWalletProvider(process.env.GOERLI_MNEMONIC, process.env.GOERLI_PROVIDER_URL, 0, 10, true)
 const Web3 = require('web3')
 const web3 = new Web3(provider)
@@ -11,57 +10,41 @@ let bob = provider.addresses[0]
 let alice = provider.addresses[1]
 console.log("Address of bob", bob)
 console.log("Address of alice", alice)
-// const SuperfluidSDK = require("./js-sdk")
 const SuperfluidSDK = require("@superfluid-finance/ethereum-contracts");
-const sf = new SuperfluidSDK.Framework({ version: "0.1.2-preview-20201014-fix6", web3Provider: provider, isTruffle: false })
-sf.initialize()
 
-// // const account = web3.eth.accounts.create()
-// // console.log(web3.eth.personal._provider.addresses[0])
-// // web3.eth.getBalance(bob, async (err, result) => {
-// //     if (err) {
-// //         console.log(err);
-// //         return;
-// //     }
-// //     let balance = web3.utils.fromWei(result, "ether");
-// //     console.log(balance + " ETH");
-// // });
+// daiaddress '0x88271d333C72e51516B67f5567c728E702b3eeE8'
 
-
-const daiaddress = '0x88271d333C72e51516B67f5567c728E702b3eeE8'
-// console.log("daiaddress: " , daiaddress)
-const SuperfluidABI = require("./build/abi")
-// const contractNames = Object.keys(SuperfluidABI)
-// console.log(contractNames)
-// let contracts = TruffleContract({
-//         contractName: i,
-//         abi: SuperfluidABI[i]
-//     })
-
-
-let Dai = sf.contracts.TestToken.at("tokens.fDAI")//.then( res => res).catch(err => console.log(err))
-//...
-
-
-
-
-const dai = () => {
-    Dai
-    .then(ok => {
-        return ok
+let sf
+let dai
+let daix
+let balanceOfBob
+(async () => {
+    sf = new SuperfluidSDK.Framework({
+    chainId: 5,
+    //version: "master",
+    version: "0.1.2-preview-20201014",
+    web3Provider: provider
     })
-    .catch(err => {
-        console.error(err)
-    })
-}
-// console.log(typeof dai)
-// const wrapper = sf.getERC20Wrapper(dai)
-/*To enjoy later also in bot.js*/
-// web3.eth.getBalance(bob, async (err, result) => {
-//     if (err) {
-//         console.log(err)
-//         return
-//     }
-//     let balance = web3.utils.fromWei(result, "ether")
-//     console.log(balance + " ETH")
-// })
+    await sf.initialize()
+    const daiAddress = await sf.resolver.get("tokens.fDAI")
+    console.log("fDai Address:", daiAddress)
+    dai = await sf.contracts.TestToken.at(daiAddress)
+    const daixWrapper = await sf.getERC20Wrapper(dai)
+    daix = await sf.contracts.ISuperToken.at(daixWrapper.wrapperAddress)
+    dai.balanceOf(bob).then( 
+        balance => {
+            balanceOfBob = wad4human(balance)
+            console.log("Bob's fDai balance:", balanceOfBob)
+        }
+    ).catch( err => console.log(err) )
+})();
+
+
+web3.eth.getBalance(bob, async (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    let balance = web3.utils.fromWei(result, "ether")
+    console.log("Bob's Balance ETH" + balance )
+})
